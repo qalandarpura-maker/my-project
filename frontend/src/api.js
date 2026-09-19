740a7b0854b0733cc8ad7b7c080f001427a3f0fd
qalandarpura-maker <qalandarpura@gmail.com>
2026-09-19 07:44:41 +0500

    Refactor API request and response interceptors

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 
-import axios from "axios";
-
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "/api",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
 api.interceptors.response.use(
-  (r) => r,
-  (err) => {
-    if (err.response?.status === 401) {
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
 
@@ -24,23 +26,6 @@ api.interceptors.response.use(
       }
     }
 
-    return Promise.reject(err);
-  }
-);
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-api.interceptors.response.use(
-  (r) => r,
-  (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      if (window.location.pathname !== "/login") window.location.href = "/login";
-    }
-    return Promise.reject(err);
+    return Promise.reject(error);
   }
 );
